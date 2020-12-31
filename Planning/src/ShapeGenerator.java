7c41d9b09a4b4eb58af04d651a072af3138b694b
JoeSubbi <joe.subbiani@gmail.com>
2020-12-31 17:27:06 +0000

    Modified ShapeGenerator to create 3cubes and 4cubes based off of a minor and major coordinate sith offset
    minor coordinate for a unit cube would be -1 meaning a bottom corner would be (-1,-1,-1) and a major coordinate would be 1 with a top corner of (1,1,1). The coordinates also now take an offset so if you wanted the cubes bottom face on z=0 for a unit cube you would have a z offset of 1 meaning a bottom corner for a unit cube could now be (-1,-1,-1) and a top corner would become (1,1,2)

--- Planning/src/ShapeGenerator.java
+++ Planning/src/ShapeGenerator.java
@@ -1,9 +1,23 @@
 public class ShapeGenerator {
 
-    public static void unit3Cube(){
+    public static ThreeObject ThreeCube(double minor, double major, double xOffset, double yOffset, double zOffset){
+        /* Unit 3Cube
+        Vector4[] coords = new Vector4[]{
+                new Vector4(-1,-1,-1), new Vector4(1,-1,-1), new Vector4(1,1,-1),new Vector4(-1,1,-1),
+                new Vector4(-1,-1,1), new Vector4(1,-1,1), new Vector4(1,1,1), new Vector4(-1,1,1),
+        };*/
+
         Vector3[] coords = new Vector3[]{
-                new Vector3(-1,-1,-1), new Vector3(1,-1,-1), new Vector3(1,1,-1),new Vector3(-1,1,-1),
-                new Vector3(-1,-1,1), new Vector3(1,-1,1), new Vector3(1,1,1), new Vector3(-1,1,1)
+                //Bottom Square
+                new Vector3(minor+xOffset,minor+yOffset,minor+zOffset),
+                new Vector3(major+xOffset,minor+yOffset,minor+zOffset),
+                new Vector3(major+xOffset,major+yOffset,minor+zOffset),
+                new Vector3(minor+xOffset,major+yOffset,minor+zOffset),
+                //Top Square
+                new Vector3(minor+xOffset,minor+yOffset,major+zOffset),
+                new Vector3(major+xOffset,minor+yOffset,major+zOffset),
+                new Vector3(major+xOffset,major+yOffset,major+zOffset),
+                new Vector3(minor+xOffset,major+yOffset,major+zOffset)
         };
         Vector3[][] faces = new Vector3[][]{
                 {coords[4],coords[5],coords[6],coords[7]},//top (+z)
@@ -13,11 +27,11 @@ public class ShapeGenerator {
                 {coords[3],coords[0],coords[4],coords[7]},//south (-x)
                 {coords[2],coords[3],coords[7],coords[6]},//west (+y)
         };
-
-        ThreeObject unitCube = new ThreeObject(coords, faces);
+        return new ThreeObject(coords, faces);
     }
 
-    public static void unit4Cube(){
+    public static FourObject FourCube(double minor, double major, double xOffset, double yOffset, double zOffset, double wOffset){
+        /* Unit 4Cube
         Vector4[] coords = new Vector4[]{
                 //"Outer" "3D" Cube
                 new Vector4(-1,-1,-1,-1), new Vector4(1,-1,-1,-1), new Vector4(1,1,-1,-1),new Vector4(-1,1,-1,-1),
@@ -25,7 +39,31 @@ public class ShapeGenerator {
                 //"Inner" "4D" Cube
                 new Vector4(-1,-1,-1,1), new Vector4(1,-1,-1,1), new Vector4(1,1,-1,1),new Vector4(-1,1,-1,1),
                 new Vector4(-1,-1,1,1), new Vector4(1,-1,1,1), new Vector4(1,1,1,1), new Vector4(-1,1,1,1)
+        };*/
 
+        Vector4[] coords = new Vector4[]{
+                //"Outer" "3D" Cube
+                //Bottom Square
+                new Vector4(minor+xOffset,minor+yOffset,minor+zOffset, minor+wOffset),
+                new Vector4(major+xOffset,minor+yOffset,minor+zOffset, minor+wOffset),
+                new Vector4(major+xOffset,major+yOffset,minor+zOffset, minor+wOffset),
+                new Vector4(minor+xOffset,major+yOffset,minor+zOffset, minor+wOffset),
+                //Top Square
+                new Vector4(minor+xOffset,minor+yOffset,major+zOffset, minor+wOffset),
+                new Vector4(major+xOffset,minor+yOffset,major+zOffset, minor+wOffset),
+                new Vector4(major+xOffset,major+yOffset,major+zOffset, minor+wOffset),
+                new Vector4(minor+xOffset,major+yOffset,major+zOffset, minor+wOffset),
+                //"Inner" "4D" Cube
+                //Bottom Square
+                new Vector4(minor+xOffset,minor+yOffset,minor+zOffset, major+wOffset),
+                new Vector4(major+xOffset,minor+yOffset,minor+zOffset, major+wOffset),
+                new Vector4(major+xOffset,major+yOffset,minor+zOffset, major+wOffset),
+                new Vector4(minor+xOffset,major+yOffset,minor+zOffset, major+wOffset),
+                //Top Square
+                new Vector4(minor+xOffset,minor+yOffset,major+zOffset, major+wOffset),
+                new Vector4(major+xOffset,minor+yOffset,major+zOffset, major+wOffset),
+                new Vector4(major+xOffset,major+yOffset,major+zOffset, major+wOffset),
+                new Vector4(minor+xOffset,major+yOffset,major+zOffset, major+wOffset)
         };
         Vector4[][] faces = new Vector4[][]{
                 //"Outer" "3D" Cube (-w)
@@ -59,6 +97,26 @@ public class ShapeGenerator {
                 {coords[14],coords[11],coords[2],coords[6]},//west north
         };
 
-        FourObject unitCube = new FourObject(coords, faces);
+        return new FourObject(coords, faces);
+    }
+
+    public static void unit3Cube(){
+        ThreeCube(-1,1,0,0,0);
+    }
+
+    public static void FourByFourPositiveZ3Cube(){
+        // bottom corner is (-2,-2,-2), top corner is (2,2,2)
+        // with z offset of 2 the bottom corner will be (-2,-2,0), top corner will be (2,2,4)
+        ThreeCube(-2,2,0,0,2);
+    }
+
+    public static void unit4Cube(){
+        FourCube(-1,1,0,0,0, 0);
+    }
+
+    public static void FourByFourPositiveZW4Cube(){
+        // bottom corner is (-2,-2,-2,-2), top corner is (2,2,2,2)
+        // with zw offset of 2 the bottom corner will be (-2,-2,0,0), top corner will be (2,2,4,4)
+        FourCube(-2,2,0,0,2, 2);
     }
 }
